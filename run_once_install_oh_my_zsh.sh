e855ce91bd0260d78b452432d5a92447c0f8d341
Trey Saddler <treyosaddler@gmail.com>
2026-06-10 11:47:09 -0400

    feat: Add oh-my-zsh automatic installer

---
+++ run_once_install_oh_my_zsh.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "Installing Oh My Zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+else
+  echo "Oh My Zsh is already installed."
+fi
