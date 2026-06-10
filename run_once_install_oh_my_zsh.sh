cd3bdbea38213b8a9d079434a3829b6762f26573
Trey Saddler <treyosaddler@gmail.com>
2026-06-10 11:55:15 -0400

    feat: install zsh-vi-mode automatically in oh-my-zsh setup script

--- run_once_install_oh_my_zsh.sh
+++ run_once_install_oh_my_zsh.sh
@@ -5,3 +5,11 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 else
   echo "Oh My Zsh is already installed."
 fi
+
+# Install custom plugins
+ZSH_CUSTOM_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+VI_MODE_DIR="$ZSH_CUSTOM_DIR/plugins/zsh-vi-mode"
+if [ ! -d "$VI_MODE_DIR" ]; then
+  echo "Installing zsh-vi-mode..."
+  git clone https://github.com/jeffreytse/zsh-vi-mode.git "$VI_MODE_DIR"
+fi
